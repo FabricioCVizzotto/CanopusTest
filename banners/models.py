47163b0796349c678aeeb7d867f7ecf0363cde53
Fabrício Castro Vizzotto <fabricio.castro.vizzotto@gmail.com>
2022-04-10 14:12:47 -0300

    Add models to Banner back-end

--- banners/models.py
+++ banners/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class SavedImage(models.Model):
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="uploads/",storage=None, max_length=10240)
+
+
+class Banner(models.Model):
+    description = models.CharField(max_length=200)
+    images = models.ManyToManyField(SavedImage)
